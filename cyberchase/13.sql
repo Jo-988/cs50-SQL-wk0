c4adc03ce6c2a302f3674e34f015637b63a0dc65
Jo-988 <wang.jianqiu@yahoo.com>
2026-08-10 16:07:07 +0000

    Mon, Aug 10, 2026, 5:07 PM +01:00

--- cyberchase/13.sql
+++ cyberchase/13.sql
@@ -1,3 +1,6 @@
-SELECT season, COUNT(*) AS episode_count
-FROM episodes
-GROUP BY season;
+SELECT AVG(episode_count)
+FROM(
+    SELECT season, COUNT(*) AS episode_count
+    FROM episodes
+    GROUP BY season
+);
